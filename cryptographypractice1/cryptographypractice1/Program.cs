b767901b5b4d4d79a9a6444407d8f945515a94c0
drDanik <SKADA2012@yandex.ru>
2022-03-01 18:19:58 +0300

    gamming
    Added encryption and decryption by gamming using pseudo-random gamming

--- cryptographypractice1/cryptographypractice1/Program.cs
+++ cryptographypractice1/cryptographypractice1/Program.cs
@@ -6,6 +6,7 @@ namespace cryptographypractice1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, world!");
             #region
             kod_vizhinera newKod = new kod_vizhinera("М", "к");
             /* newKod.Encryption("ATTACKATDAWN", "LEMONLEMONLE");
@@ -13,8 +14,13 @@ namespace cryptographypractice1
             #endregion
             #region
             Permutation newKod2 = new Permutation();
-            newKod2.Encryption("москва", "412035");
-            newKod2.Deryption("косвма", "412035");
+            //newKod2.Encryption("москва", "412035");
+           // newKod2.Deryption("косвма", "412035");
+            #endregion
+            #region
+            Xor xor = new Xor();
+            //xor.Encryption("HELLO");
+            xor.Decryption("DIWTD", "WDKHP");
             #endregion
         }
     }
--- cryptographypractice1/cryptographypractice1/Xor.cs
+++ cryptographypractice1/cryptographypractice1/Xor.cs
@@ -8,5 +8,73 @@ namespace cryptographypractice1
 {
     class Xor
     {
+        private static string dictionatry = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public void Encryption(string input)
+        {           
+            input.Insert(0, " ");
+           byte[] mass1 =  WordNumbering(input);
+           byte[] mass2 =  Random(input);
+           byte[] mass3 = new byte[input.Length];
+           string output = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (mass1[i] + mass2[i] >= 27) mass3[i] = (byte)((mass1[i] + mass2[i]) - (byte)dictionatry.Length);
+                else
+                mass3[i] = (byte)(mass1[i] + mass2[i]);
+                output += dictionatry[mass3[i]];
+            }
+            Console.WriteLine(output);
+        }
+
+        private byte[] WordNumbering(string input)
+        {
+            input.ToUpper();
+            char[] slovar = dictionatry.ToCharArray();
+            byte[] mass = new byte[input.Length];
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                int positionOfWord = dictionatry.IndexOf(input[j]);
+                if (dictionatry.IndexOf(input[j]) == -1)
+                {
+                    //значения не соответсвующие словарю итгнорируются? как лучше сделать оставлять пробелы и знаки препинания или игнорировать?
+                    continue;
+                }
+                else
+                    mass[j] = (byte)positionOfWord;
+            }
+
+            return mass;
+        }
+        public byte[] Random(string input)
+        {
+            Random rnd = new Random();
+            string key = string.Empty;
+            byte[] arr = new byte[input.Length];            
+            for (int i = 0; i < input.Length; i++)
+            {
+                int value = rnd.Next(1, 26);
+                arr[i] = (byte)value;
+                key += dictionatry[value];
+            }
+            Console.WriteLine("Key is:" + key);
+            return arr;
+        }
+
+        public void Decryption(string shifr, string key)
+        {
+            byte[] mass1 = WordNumbering(shifr);
+            byte[] mass2 = WordNumbering(key);
+            byte[] mass3 = new byte[shifr.Length];
+            string output = string.Empty;
+            for (int i = 0; i < shifr.Length; i++)
+            {
+                if (mass1[i] - mass2[i] <= 0)  mass3[i] = (byte)((mass1[i] - mass2[i]) + (byte)dictionatry.Length);
+                else
+                    mass3[i] = (byte)(mass1[i] - mass2[i]);
+                output += dictionatry[mass3[i]];
+            }
+            Console.WriteLine(output);
+        }
     }
 }
